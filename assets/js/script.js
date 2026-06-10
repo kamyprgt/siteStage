4e936e2e06de2a835a47736fe78b9691d1da2041
kamyprgt <kprigent.ledantec@gmail.com>
2026-06-10 09:46:06 +0200

    reglage sécu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,28 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-	fetch("http://sitestagegit/assets/includes/header.html")
-		.then(response => response.text())
+	fetch("../assets/includes/header.html")
+		.then(response => {
+        if (!response.ok) {
+            throw new Error("Header introuvable");
+        }
+        return response.text();
+		})
 		.then(data => {
 			document.getElementById("header").innerHTML = data;
-		});
+		})
+		.catch(error => console.error(error));
 
 
 
-	fetch("http://sitestagegit/assets/includes/footer.html")
-		.then(response => response.text())
+	fetch("../assets/includes/footer.html")
+		.then(response => {
+        if (!response.ok) {
+            throw new Error("Footer introuvable");
+        }
+        return response.text();
+		})
 		.then(data => {
 			document.getElementById("footer").innerHTML = data;
-		});
-
-});
+		})
+		.catch(error => console.error(error));
+	});
