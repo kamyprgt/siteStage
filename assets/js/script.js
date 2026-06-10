1dce6c9f3e3c1a6b508e00332179a1bcee109bc4
kamyprgt <kprigent.ledantec@gmail.com>
2026-06-10 13:54:11 +0200

    reglages chemin 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,28 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-    const base = window.location.pathname.includes("/pages/")
-        ? "../assets/includes/"
-        : "assets/includes/";
+	fetch("assets/includes/header.html")
+		.then(response => {
+        if (!response.ok) {
+            throw new Error("Header introuvable");
+        }
+        return response.text();
+		})
+		.then(data => {
+			document.getElementById("header").innerHTML = data;
+		})
+		.catch(error => console.error(error));
 
-    fetch(base + "header.html")
-        .then(r => {
-            if (!r.ok) throw new Error("Header introuvable");
-            return r.text();
-        })
-        .then(data => {
-            document.getElementById("header").innerHTML = data;
-        })
-        .catch(console.error);
 
-    fetch(base + "footer.html")
-        .then(r => {
-            if (!r.ok) throw new Error("Footer introuvable");
-            return r.text();
-        })
-        .then(data => {
-            document.getElementById("footer").innerHTML = data;
-        })
-        .catch(console.error);
 
-});
+	fetch("assets/includes/footer.html")
+		.then(response => {
+        if (!response.ok) {
+            throw new Error("Footer introuvable");
+        }
+        return response.text();
+		})
+		.then(data => {
+			document.getElementById("footer").innerHTML = data;
+		})
+		.catch(error => console.error(error));
+	});
