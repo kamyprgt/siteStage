a12b74bd75ca9e80d64565058fe25bc2bcbd07fc
kamyprgt <kprigent.ledantec@gmail.com>
2026-06-23 17:06:02 +0200

    update barre actu

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,17 +27,36 @@ window.addEventListener("DOMContentLoaded", () => {
 		.catch(error => console.error(error));
 	
 
-	fetch("assets/json/actualites.json")
+		fetch("assets/json/actualites.json")
 		.then(response => response.json())
 		.then(data => {
 
-			const actualites = data.map(actu => {
-				return `📅 ${actu.date} • 🕒 ${actu.heure} • 📍 ${actu.lieu} • ${actu.evenement}`;
-			});
+			const aujourdHui = new Date();
 
-			document.getElementById("actualites-content").innerHTML =
-				actualites.join(" &nbsp;&nbsp;&nbsp;&nbsp; • &nbsp;&nbsp;&nbsp;&nbsp; ");
+			const prochain = data
+				.filter(actu => new Date(actu.dateISO) >= aujourdHui)
+				.sort((a, b) => new Date(a.dateISO) - new Date(b.dateISO))[0];
 
+			const container = document.getElementById("actualites-content");
+
+			if (!prochain) {
+				container.innerHTML = "Aucun événement à venir.";
+				return;
+			}
+
+			container.innerHTML = `
+				<span class="actualites-evenement">
+					${prochain.evenement}
+				</span>
+				—
+				<span class="actualites-date">
+					${prochain.date} à ${prochain.heure}
+				</span>
+				—
+				<span class="actualites-lieu">
+					📍 ${prochain.lieu}
+				</span>
+			`;
 		})
 		.catch(error => console.error(error));
 
